bb1a31dba2b426a8d54e3f6b039ed7f6ff0bdce7
kacperkapusciak <kapusciak.k@gmail.com>
2021-05-18 19:58:36 +0200

    feat: implement sequence edit

--- src/main/java/com/sequencero/app/controller/SequenceController.java
+++ src/main/java/com/sequencero/app/controller/SequenceController.java
@@ -5,6 +5,7 @@ import com.sequencero.app.model.Sequence;
 import com.sequencero.app.repository.SequenceRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +33,19 @@ public class SequenceController {
     @ResponseStatus(HttpStatus.CREATED)
     public Sequence addSequence(@RequestBody AddSequenceDto sequenceDto) {
         return sequenceRepository.insert(new Sequence(sequenceDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Sequence> editSequence(@PathVariable("id") String id, @RequestBody AddSequenceDto sequenceDto) {
+        Sequence sequence = sequenceRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sequence not found :: " + id));
+
+        sequence.setName(sequenceDto.getName());
+        String content = sequenceDto.getBody();
+        sequence.setBody(content.split("\\r?\\n"));
+        sequence.setSeqIsPublic(sequenceDto.isSeqIsPublic());
+
+        return ResponseEntity.ok(sequenceRepository.save(sequence));
     }
 
     @DeleteMapping("/{id}")
